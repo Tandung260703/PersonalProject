1002cec96b6e5d60cbd81eca76a5c6394b3f4d39
ntdg03 <133014120+Tandung260703@users.noreply.github.com>
2026-02-27 18:59:07 +0700

    Update gi

--- coffeShop/client/vite.config.js
+++ coffeShop/client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/coffeShop/",
+  base: "/PersonalProject/coffeShop/",
   server: {
     proxy: {
       '/coffe': 'https://680c90bf2ea307e081d44f4e.mockapi.io/coffeNTD/api',
